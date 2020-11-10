f839b8fa64156bd3eb9883d484504bca34d7ab1d
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-10 11:50:01 -0500

    Add a SearchBar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input @:input="onInput" type="text">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SearchBar",
+    methods: {
+        onInput: function(e){
+            console.log(e.target.value)
+        }
+   }
+};
+</script>
+
+<style scoped>
+
+</style>
